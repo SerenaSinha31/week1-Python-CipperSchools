e136586d651c0dd4fc787594debed8edd5153c23
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-01 01:44:42 +0530

    Add files via upload

---
+++ class3-var datatypes-cipperschools.py
@@ -0,0 +1,10 @@
+#type of variable
+a = "serena"
+print(type(a))
+
+#logical operators in python
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 and bool2 is",(bool1 or bool2))
+print("The value of not bool1 is",(not bool2))
