71bb3f3359dee7a69fc3f191a1c41de2176f379d
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-01 01:45:04 +0530

    Add files via upload

---
+++ class4-typecasting-cipherschools.py
@@ -0,0 +1,16 @@
+#Type Casting
+a = int(input("enter a number3 "))
+
+print(type(a))
+
+#practice question 1
+b = int(input("Enter a number:"))
+c = int(input("Enter a number:"))
+print(a+b)
+
+#practice question 2
+d = int(input("enter a number"))
+b = a%2
+print(b)
+
+
