92c8e6ce240e0eb4327e171934bc30432796f820
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-01 01:44:07 +0530

    Add files via upload

---
+++ class2-arit variablet.py
@@ -0,0 +1,23 @@
+#float division
+print(2/4)
+print(4/2)
+
+#exponent,square root
+print(2**3)
+ 
+print(2**(1/2)) 
+print(2**(1/3))
+
+#round of a number
+print(round(2**(1/2),4))
+
+#precendence rule
+print((2+3)*5/2%6)
+
+#input of a variable
+a = input("enter a name:")
+
+b = int(input("a Number:"))
+c = int(input("a number"))
+t = b+c
+print(t)
