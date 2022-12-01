4b418de9f2b7fe0c4257f283a26976f1df93d6bb
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-01 19:54:20 +0530

    Add files via upload

---
+++ class9-loops-cipherSchoola.py
@@ -0,0 +1,18 @@
+print("hello world")
+i=1
+while i<=10:
+    print(f"hello world{i}")
+    i=i+1
+#sum of 1 to 10 numbers
+total=0
+i=1
+while i<=10:
+    total=total+1
+    i=i+1
+    print(total)
+    
+
+
+
+
+
