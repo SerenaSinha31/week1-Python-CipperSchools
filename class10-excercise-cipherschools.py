9151abdc8a5746d7a21de0dc3d46d5d2ba8a88c9
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-01 20:01:25 +0530

    Add files via upload

---
+++ class10-excercise-cipherschools.py
@@ -0,0 +1,20 @@
+n = int(input("Enter a number"))
+t=0
+i=1
+while(i<n):
+    t=t+i
+    i=i+1
+print(t) 
+
+#prob 2
+n=input("more than one digit")
+t=0
+i=0
+while(i<len(n)):
+            t=t+int(n[i])
+            i=i+1
+            print(t)
+
+
+
+
